1a49f455802c0d7f61841042232683f0dcacd450
potter <potter@hwi.buffalo.edu>
2009-09-03 21:13:31 +0000

    Implementation of Savitzky-Golay filters for smoothing and differentiation by  Marcin Rze?nicki.
    git-svn-id: https://svn.dhcp.hwi.buffalo.edu/potter-src/SGFilter/trunk@223 8d77567b-513a-de11-b4d2-003048323f7e

---
+++ test/mr/go/sgfilter/tests/FilterTestCaseTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mr.go.sgfilter.tests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author potter
+ */
+public class FilterTestCaseTest {
+
+    public FilterTestCaseTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of testComputeSGCoefficients method, of class FilterTestCase.
+     */
+    @Test
+    public void testTestComputeSGCoefficients() {
+        System.out.println("testComputeSGCoefficients");
+        FilterTestCase instance = new FilterTestCase();
+        instance.testComputeSGCoefficients();
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testSmooth1 method, of class FilterTestCase.
+     */
+    @Test
+    public void testTestSmooth1() {
+        System.out.println("testSmooth1");
+        FilterTestCase instance = new FilterTestCase();
+        instance.testSmooth1();
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testSmooth2 method, of class FilterTestCase.
+     */
+    @Test
+    public void testTestSmooth2() {
+        System.out.println("testSmooth2");
+        FilterTestCase instance = new FilterTestCase();
+        instance.testSmooth2();
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testSmooth3 method, of class FilterTestCase.
+     */
+    @Test
+    public void testTestSmooth3() {
+        System.out.println("testSmooth3");
+        FilterTestCase instance = new FilterTestCase();
+        instance.testSmooth3();
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+}
